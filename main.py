01c9e3adf68df29ce3e1a648bdc313967bc18f13
Sebastian Klapdor <sk@transformx.group>
2025-05-26 20:01:39 +0200

    Use getpass instead of command line argument for enhanced security

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import getpass
 import sys
 import time
 from freeathome.register_devices import register_device
 
-if len(sys.argv) != 4:
-    print("Usage: python main.py <devices_file> <accesspoint_url> <installer_password>")
-    print("Example: python main.py devices.txt http://192.168.1.204 meinPasswort123")
+if len(sys.argv) != 3:
+    print("Usage: python main.py <devices_file> <accesspoint_url>")
+    print("Example: python main.py devices.txt http://192.168.1.204")
     sys.exit(1)
 
 devices_file = sys.argv[1]
 base_url = sys.argv[2]
-password = sys.argv[3]
+password = getpass.getpass("Please enter the installer password: ")
 
 with open(devices_file, encoding="utf-8") as f:
     for line in f:
